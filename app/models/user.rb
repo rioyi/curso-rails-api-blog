8dc3011a4ac510bedd11c95563982c3514f062ef
rioyi <wilbert.angarita@gmail.com>
2019-08-27 16:30:59 -0300

    Autenticación: generacio de auth token para el usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+  after_initialize :generate_auth_token
+
   has_many :posts
 
   validates :name, presence: true
   validates :email, presence: true
   validates :auth_token, presence: true
+
+
+  def generate_auth_token
+    self.auth_token = TokenGenerationService.generate unless auth_token.present?
+  end
 end
