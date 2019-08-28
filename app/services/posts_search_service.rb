cd445e3ec467284101c60e55091a4672e4ff25de
rioyi <wilbert.angarita@gmail.com>
2019-08-27 22:52:38 -0300

    Usando Cache de Rails
    Esto nos ayuda a guardar en memoria cache el resuldato de la busqueda en la base de datos solo una vez, y luego quedaran los ids listo para la proxima busqueda, ahorrando el tiempo de busqueda.

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,5 +1,10 @@
 class PostsSearchService
   def self.search(curr_posts, query)
-    curr_posts.where("title LIKE '%#{query}%' ")
+    # {k => v}
+    posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in: 1.hours) do
+      curr_posts.where("title LIKE '%#{query}%' ").map(&:id)
+    end
+
+    curr_posts.where(id: posts_ids)
   end
 end
